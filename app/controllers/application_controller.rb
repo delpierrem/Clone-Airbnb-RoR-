80cd6f236faa1e35832c2c58316e2ab4322587ea
delpierrem <delpierre.michel@outlook.fr>
2017-08-17 11:50:38 +0200

    Create fullname collumn into user and permit it into devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # avant toute action de devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def configure_permitted_parameters
+    # permet d'autoriser un nouveau params pour devise
+    devise_permitted_sanitizer.permit(:sign_up, key: [:fullname])
+    devise_permitted_sanitizer.permit(:account_update, key: [:fullname])
+  end
+
 end
