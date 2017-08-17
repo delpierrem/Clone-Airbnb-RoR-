e640e2a5cdfee7c1eeb0acb7420620b6bbececdd
delpierrem <delpierre.michel@outlook.fr>
2017-08-17 15:21:19 +0200

    fix device parameter sanitizer and change device views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # avant toute action de devise
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
-
   def configure_permitted_parameters
-    # permet d'autoriser un nouveau params pour devise
-    devise_permitted_sanitizer.permit(:sign_up, key: [:fullname])
-    devise_permitted_sanitizer.permit(:account_update, key: [:fullname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
   end
-
 end
