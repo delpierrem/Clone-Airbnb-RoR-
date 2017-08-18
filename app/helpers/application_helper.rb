eae8403acef21f1295a8dad8946572f6dca02604
delpierrem <delpierre.michel@outlook.fr>
2017-08-18 16:12:48 +0200

    change avatar_url helper into appHelp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
-def avatar_url(user)
-  user.avatar.url
-end
+  def avatar_url(user)
+    if user.avatar.present?
+      user.avatar.url
+    else
+      'default_image.png'
+    end
+  end
 
 end
