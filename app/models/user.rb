c8b963b7e339ff25475d9159e76f441317ecb009
delpierrem <delpierre.michel@outlook.fr>
2017-08-18 14:19:04 +0200

    create rooms Model and add some valitades

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :fullname, presence:true, length:{maximum: 50}
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default_image.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  has_many :rooms
 end
