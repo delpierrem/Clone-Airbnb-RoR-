9b01b60a4e9b3fc6675409fa0faaf713e31d3464
delpierrem <delpierre.michel@outlook.fr>
2017-08-22 11:09:47 +0200

    add search method with ransak

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
   get '/your_trips' => 'reservations#your_trips' , path:'mes_voyages'
-
+  get '/search' => 'pages#search'
 
 end
