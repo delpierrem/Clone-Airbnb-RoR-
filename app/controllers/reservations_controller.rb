19139d065e7dc779aa85035b8502c5b509283e1c
delpierrem <delpierre.michel@outlook.fr>
2017-08-28 10:01:25 +0200

    add mailer for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,7 +24,10 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = current_user.reservations.create(reservation_params)
-    redirect_to @reservation.room, notice:  "Votre réservation a été acceptée"
+    if @reservation.save
+      AppMailer.new_reservation(Room.find(@reservation.room_id), @reservation).deliver_now
+      redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
+    end
   end
 
   private
